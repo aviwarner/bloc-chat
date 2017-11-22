f4a73c06f47d46960361eb16ae74e908a19035f9
Avi Warner <aviwarner@gmail.com>
2017-11-21 21:13:03 -0800

    Finished checkpoint 2 story

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,9 @@
 (function() {
-  function HomeCtrl() {
-
+  function HomeCtrl($scope, Room) {
+    $scope.rooms = Room;
   }
 
   angular
     .module('blocChat')
-    .controller('HomeCtrl', [HomeCtrl]);
+    .controller('HomeCtrl', ['$scope', 'Room', HomeCtrl]);
 })();

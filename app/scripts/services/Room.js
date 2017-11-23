04428b0b4ecf4ab713f6e9f0edb23b9f424bda75
Avi Warner <aviwarner@gmail.com>
2017-11-22 17:18:00 -0800

    New room button creates test room

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -5,6 +5,16 @@
     var rooms = $firebaseArray(ref);
     Room.all = rooms;
 
+    Room.add = function(room) {
+      rooms.$add(room).then(function(ref) {
+        var id = ref.key;
+        console.log("added record with id " + id);
+        rooms.$indexFor(id); // returns location in the array
+        console.log(rooms.$indexFor(id));
+      });
+
+    }
+
     return Room;
   }
 
